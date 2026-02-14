93cc1f1fa82c91999fe2e8388a4131d790ad1c42
earlbug <erlendsundsdal@gmail.com>
2026-02-14 17:22:58 +0100

    add input validation

--- src/components/FeedbackForm.vue
+++ src/components/FeedbackForm.vue
@@ -1,34 +1,32 @@
-<script setup>
-import BaseInput from './BaseInput.vue';
-import BaseTextField from './BaseTextField.vue';
-
-</script>
-
 <template >
-  <form @submit.prevent="sendForm">
+  <form @submit.prevent="onSubmit">
 
     <BaseInput
-    v-model="feedbackEntry.name"
+    v-model="name"
     label="Name"
     type="text"
+    :error="nameError"
     />
 
     <BaseInput
-    v-model="feedbackEntry.email"
+    v-model="email"
     label="E-mail"
     type="text"
+    :error="emailError"
     />
 
     <BaseTextField
-    v-model="feedbackEntry.feedback"
+    v-model="feedback"
     label="Feedback"
     type="text"
+    :error="feedbackError"
     />
+
 
     <button type="submit">Submit</button>
   </form>
 
-  <pre>{{feedbackEntry}}</pre>
+  <pre>{{name}}</pre>
 
 </template>
 
@@ -36,40 +34,63 @@ import BaseTextField from './BaseTextField.vue';
 import axios from 'axios'
 import { useField } from 'vee-validate'
 export default {
-  setup() {
+  setup () {
     function onSubmit () {
-      alert('Ubmitted')
+      alert('sUbmitted')
     }
 
+    const name = useField('name', function(value) {
+      if (!value) return 'This field is required'
+      return true
+    })
 
-    return {onSubmit}
-  },
-  data () {
-    return {
-      feedbackEntry: {
-        name: 'erl',
-        email: 'wggr@',
-        feedback: 'work not'
+    const email = useField('email', function (value) {
+      if (!value) return 'This field is required'
+
+      const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      if(!regex.test(String(value).toLocaleLowerCase())) {
+        return 'Please enter a valid e-mail address'
       }
-    }
-  },
-  methods: {
-    sendForm () {
-      // important to validate
-      //  Axios sends a promise
-      axios.post(
-        // url to where to post data to
-        'http://localhost:3000/feedbackEntry',
-        //payload to send to server
-        this.feedbackEntry
-      )
-      .then(function (response) {
-        console.log('Response', response)
-      })
-      .catch(function (err) {
-      console.log('Error', err)
-      })
+      return true
+    })
+
+    const feedback = useField('feedback', function(value) {
+      if (!value) return 'This field is required'
+      return true
+    })
+
+    return {
+      onSubmit,
+      name: name.value,
+      nameError: name.errorMessage,
+      email: email.value,
+      emailError: email.errorMessage,
+      feedback: feedback.value,
+      feedbackError: feedback.errorMessage
     }
   }
+
+
+  /*,
+  sendForm () {
+  // important to validate
+  //  Axios sends a promise
+  axios.post(
+    // url to where to post data to
+    'http://localhost:3000/feedbackEntry',
+    //payload to send to server
+    this.feedbackEntry
+  )
+  .then(function (response) {
+    console.log('Response', response)
+  })
+  .catch(function (err) {
+  console.log('Error', err)
+  })
+}
+  */
+
+
+
 }
 </script>
