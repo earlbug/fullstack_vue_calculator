f49e6238eab904c8233ad09c2152257dd58225da
earlbug <erlendsundsdal@gmail.com>
2026-02-17 22:49:19 +0100

    refactor feedbackform to use omly composition api

--- src/components/FeedbackForm.vue
+++ src/components/FeedbackForm.vue
@@ -32,114 +32,98 @@
 
 </template>
 
-<script>
-import axios from 'axios'
-import { useField, useForm } from 'vee-validate'
-import {useUserStore} from '../stores/counter'
-export default {
-  setup () {
+<script setup>
+  import axios from 'axios'
+  import { useField, useForm } from 'vee-validate'
+  import {useUserStore} from '../stores/counter'
 
-    const  userStore  = useUserStore();
+  const  userStore  = useUserStore();
 
-
-    const validations = {
-      name: value => {
+  const validations = {
+    name: value => {
+    if (!value) return 'This field is required'
+    return true
+    },
+    email: value=> {
+      if (!value) return 'This field is required'
+      const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      if(!regex.test(String(value).toLocaleLowerCase())) {
+        return 'Please enter a valid e-mail address'
+    }
+    return true
+    },
+    feedback: value=> {
       if (!value) return 'This field is required'
       return true
-      },
-      email: value=> {
-        if (!value) return 'This field is required'
-        const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-        if(!regex.test(String(value).toLocaleLowerCase())) {
-          return 'Please enter a valid e-mail address'
-      }
-      return true
-      },
-      feedback: value=> {
-        if (!value) return 'This field is required'
-        return true
 
-      }
     }
+  }
 
-    const {handleSubmit} = useForm({
-      validationSchema: validations
+  const {handleSubmit} = useForm({
+    validationSchema: validations
+  })
+
+  const name = useField('name')
+  const email = useField('email')
+  const feedback = useField('feedback')
+  /*
+  const feedback = useField('feedback', function(value) {
+    if (!value) return 'This field is required'
+    return true
     })
+  */
 
-    const name = useField('name')
-    const email = useField('email')
-    const feedback = useField('feedback')
-    /*
-    const feedback = useField('feedback', function(value) {
-      if (!value) return 'This field is required'
-      return true
-      })
-    */
+  function sendForm (values) {
 
-
-
-    function sendForm (values) {
-
-      // important to validate
-      //  Axios sends a promise
-      axios.post(
-        // url to where to post data to
-        'http://localhost:3000/feedbackEntry',
-        //payload to send to server
-        values
-      )
-      .then(function (response) {
-        // log the response on success
-        console.log('Response', response)
-      })
-      .catch(function (err) {
-      console.log('Error', err)
-      })
-    }
-
-    // TODO try to getg the values from the passed on argument
-    function storeUser(values) {
-      // Store the user data in state
-      userStore.setUsername(name.value.value)
-      userStore.setEmail(email.value.value)
-    }
-
-
-    const submit = handleSubmit(values => {
-    console.log('submit', values)
-    storeUser(values)
-    sendForm(values)
+    // important to validate
+    //  Axios sends a promise
+    axios.post(
+      // url to where to post data to
+      'http://localhost:3000/feedbackEntry',
+      //payload to send to server
+      values
+    )
+    .then(function (response) {
+      // log the response on success
+      console.log('Response', response)
     })
+    .catch(function (err) {
+    console.log('Error', err)
+    })
+  }
 
-    const recallUser = function tesst() {
-      const storedUserName = userStore.getUsername;
-      const storedEmail = userStore.getEmail;
+  // TODO try to getg the values from the passed on argument
+  function storeUser(values) {
+    // Store the user data in state
+    userStore.setUsername(name.value.value)
+    userStore.setEmail(email.value.value)
+  }
 
+  // validate fields, send user info to store and send the form
+  const submit = handleSubmit(values => {
+  console.log('submit', values)
+  storeUser(values)
+  sendForm(values)
+  })
 
-      if(storedUserName != '' && storedEmail != '') {
-        name.value.value = storedUserName;
-        email.value.value = storedEmail;
-      }
-
+  // Get info form store, and put them in the fields if they exixt.
+  const recallUser = function tesst() {
+    const storedUserName = userStore.getUsername;
+    const storedEmail = userStore.getEmail;
+    if(storedUserName != '' && storedEmail != '') {
+      name.value.value = storedUserName;
+      email.value.value = storedEmail;
     }
 
-
-
-
-
-    return {
-      name: name.value,
-      nameError: name.errorMessage,
-      email: email.value,
-      emailError: email.errorMessage,
-      feedback: feedback.value,
-      feedbackError: feedback.errorMessage,
-      submit,
-      recallUser
-    }
   }
 
 
 
-}
+
+
+
+
+
+
+
 </script>
