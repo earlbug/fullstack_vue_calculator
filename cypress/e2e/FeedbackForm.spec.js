dfbd66daa6f476aa5a76e060f89996b167ee5440
earlbug <erlendsundsdal@gmail.com>
2026-02-19 14:01:19 +0100

    add server response check

--- cypress/e2e/FeedbackForm.spec.js
+++ cypress/e2e/FeedbackForm.spec.js
@@ -6,6 +6,10 @@ describe('Feedback form e2e', () => {
   it('has submit disabled then enables after filling fields', () => {
     cy.visit('http://localhost:5173/feedback')
     // find submit button
+
+    cy.intercept('post', 'https://my-json-server.typicode.com/earlbug/fullstack_vue_calculator/feedbackEntry')
+    .as('postFeedback')
+
     cy.contains('button', 'Submit').should('be.disabled')
 
     // fill name and email and feedback
@@ -15,5 +19,20 @@ describe('Feedback form e2e', () => {
 
     // submit should be enabled
     cy.contains('button', 'Submit').should('not.be.disabled')
+
+    cy.contains('button', 'Submit').click()
+
+    cy.wait('@postFeedback').then(({ request, response }) => {
+      // assert request payload
+      expect(request.body).to.include({
+        name: 'Test User',
+        email: 'test@example.com',
+        feedback: 'Nice app'
+      })
+
+      // assert response contains the sent values (if the server echoes them)
+      expect(response && response.body).to.include(request.body)
+
   })
+})
 })
