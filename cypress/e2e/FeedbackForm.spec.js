90a782aac9f72d76e5ef10f2722a89e6ecdd1865
earlbug <erlendsundsdal@gmail.com>
2026-02-19 13:24:26 +0100

    make feedbackform e2e test with cypress

---
+++ cypress/e2e/FeedbackForm.spec.js
@@ -0,0 +1,19 @@
+describe('Feedback form e2e', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:5173/')
+  })
+
+  it('has submit disabled then enables after filling fields', () => {
+    cy.visit('http://localhost:5173/feedback')
+    // find submit button
+    cy.contains('button', 'Submit').should('be.disabled')
+
+    // fill name and email and feedback
+    cy.get('input[placeholder="Name"]').type('Test User')
+    cy.get('input[placeholder="E-mail"]').type('test@example.com')
+    cy.get('textarea[placeholder="Feedback"]').type('Nice app')
+
+    // submit should be enabled
+    cy.contains('button', 'Submit').should('not.be.disabled')
+  })
+})
