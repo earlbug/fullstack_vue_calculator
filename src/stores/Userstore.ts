d984eb2cbe6df6ec050eb7fc6f4b58f46987ae3c
earlbug <erlendsundsdal@gmail.com>
2026-03-04 15:08:55 +0100

    add userstore

---
+++ src/stores/Userstore.ts
@@ -0,0 +1,29 @@
+import { defineStore } from 'pinia'
+
+import { getJwtToken } from '../utils/authService'
+
+export const useUserStore = defineStore('token', {
+    state: () => ({
+      jwtToken: '',
+      loggedInUser: '',
+    }),
+
+    actions: {
+      async getTokenAndSaveInStore(username: string, password: string) {
+        try {
+          await getJwtToken(username, password).then((token) => {
+            if (token) {
+              console.log('JWT Token:', token)
+              this.jwtToken = token
+              this.loggedInUser = username
+            } else {
+              console.log('Failed to retrieve token')
+            }
+          })
+        } catch (err) {
+          console.log(err)
+        }
+      },
+    },
+
+  })
